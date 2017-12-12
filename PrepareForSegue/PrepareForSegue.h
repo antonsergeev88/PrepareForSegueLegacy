1b6fd98aeb97cd7de375f4c0fcf39b148745d831
Anton Sergeev <antonsergeev88@gmail.com>
2017-12-12 08:23:15 +0300

    Initial Commit

---
+++ PrepareForSegue/PrepareForSegue.h
@@ -0,0 +1,19 @@
+//
+//  PrepareForSegue.h
+//  PrepareForSegue
+//
+//  Created by Anton Sergeev on 12.12.2017.
+//  Copyright © 2017 Anton Sergeev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PrepareForSegue.
+FOUNDATION_EXPORT double PrepareForSegueVersionNumber;
+
+//! Project version string for PrepareForSegue.
+FOUNDATION_EXPORT const unsigned char PrepareForSegueVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PrepareForSegue/PublicHeader.h>
+
+
